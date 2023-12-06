709266d8afe1c00de7fe85e8d6ac5033da8763a2
壹零 <guchengxi1994@qq.com>
2023-12-06 10:23:45 +0800

    feat: move rust to `fsb`

--- lib/bridge/native.dart
+++ lib/bridge/native.dart
@@ -1,17 +1,17 @@
 import 'dart:ffi';
 
-import 'bridge_generated.dart';
+import 'package:fsb_dart/bridge_generated.dart';
+export 'package:fsb_dart/bridge_generated.dart';
+export 'package:fsb_dart/bridge_definitions.dart';
 
-// Re-export the bridge so it is only necessary to import this file.
-export 'bridge_generated.dart';
 import 'dart:io' as io;
 
-const _base = 'native';
+const _base = 'fsb';
 
 // On MacOS, the dynamic library is not bundled with the binary,
 // but rather directly **linked** against the binary.
 final _dylib = io.Platform.isWindows ? '$_base.dll' : 'lib$_base.so';
 
-final api = NativeImpl(io.Platform.isIOS || io.Platform.isMacOS
+final api = FsbImpl(io.Platform.isIOS || io.Platform.isMacOS
     ? DynamicLibrary.executable()
     : DynamicLibrary.open(_dylib));
