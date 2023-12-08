e8f17a035995ee3cec98deb00401f2dc89b07a9b
xiaoshuyui <528490652@qq.com>
2023-12-08 22:50:30 +0800

    upload dll

--- lib/settings/components/common_setting.dart
+++ lib/settings/components/common_setting.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:fsb_dart/bridge_definitions.dart';
 import 'package:weaving/bridge/native.dart';
 import 'package:weaving/gen/strings.g.dart';
@@ -8,11 +9,27 @@ import 'package:weaving/notifier/color_notifier.dart';
 import 'package:weaving/notifier/settings_notifier.dart';
 import 'package:weaving/style/app_style.dart';
 
-class CommonSettingWidget extends ConsumerWidget {
+class CommonSettingWidget extends ConsumerStatefulWidget {
   const CommonSettingWidget({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CommonSettingWidget> createState() =>
+      _CommonSettingWidgetState();
+}
+
+class _CommonSettingWidgetState extends ConsumerState<CommonSettingWidget> {
+  late final Stream<String> stream = api.dartMessageStream();
+
+  @override
+  void initState() {
+    super.initState();
+    stream.listen((event) {
+      SmartDialog.showToast(event);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.all(20),
       child: Column(
@@ -93,14 +110,54 @@ class CommonSettingWidget extends ConsumerWidget {
             ),
             ElevatedButton(
                 onPressed: () {
-                  api.showNotification(
+                  api.showDialog(
                       message: const EventMessage(
                           title: "a ba a ba",
-                          content: "a ba a ba",
+                          content: "居中",
                           alignment: (0, 0),
                           dialogType: DialogType.notification));
                 },
-                child: Text("test"))
+                child: Text("居中通知")),
+            ElevatedButton(
+                onPressed: () {
+                  api.showDialog(
+                      message: const EventMessage(
+                          title: "a ba a ba",
+                          content: "右下",
+                          alignment: (1, 1),
+                          dialogType: DialogType.notification));
+                },
+                child: Text("右下通知")),
+            ElevatedButton(
+                onPressed: () {
+                  api.showDialog(
+                      message: const EventMessage(
+                          title: "a ba a ba",
+                          content: "左上",
+                          alignment: (-1, -1),
+                          dialogType: DialogType.notification));
+                },
+                child: Text("左上通知")),
+            ElevatedButton(
+                onPressed: () {
+                  api.showDialog(
+                      message: const EventMessage(
+                          title: "Dialog",
+                          content: "确认否？",
+                          alignment: (0, 0),
+                          dialogType: DialogType.confirmDialog));
+                },
+                child: Text("确认弹窗")),
+            ElevatedButton(
+                onPressed: () {
+                  api.showDialog(
+                      message: const EventMessage(
+                          title: "Dialog",
+                          content: "确认否？",
+                          alignment: (0, 0),
+                          dialogType: DialogType.warningDialog));
+                },
+                child: Text("操作")),
           ]),
     );
   }
